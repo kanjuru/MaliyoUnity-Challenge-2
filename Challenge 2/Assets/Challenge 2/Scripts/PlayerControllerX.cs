7902a38bb19355a1e2ae581b119561b48300e0d4
Kanjuru Wahome <testy9485@gmail.com>
2021-09-11 13:22:56 +0300

    Code and gameplay recording challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float startTime;
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - startTime >= 2)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            startTime = Time.time;
+        }
+     
+    }
+   
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# MaliyoUnity
+##Contains Maliyo studio project challenge 1.
+
+
+The plane moves up and down from keyboard input using the up and down arrow keys.
+The plane's rotor spins.
+The plane can also spins on keyboard inputs  from the left and right arrow keys.
